337c6930525865b96178d6df4a1ec74005719aa4
WilfredKesh <wilfredgish5@gmail.com>
2016-08-01 23:03:12 -0400

    added api controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+ 
+ root 'page#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ namespace :api do 
+  namespace :v1 do 
+    resources :items, only: [:index, :create, :destroy, :update ]
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
